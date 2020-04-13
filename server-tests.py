878015ce349ae35a06d2fe04103ea40bd8ff5e32
Ha Ly <lyha@pdx.edu>
2020-04-13 00:33:06 -0700

    Refactor

--- server-tests.py
+++ server-tests.py
@@ -12,11 +12,12 @@ class TestServerApp(unittest.TestCase):
 
     def setUp(self):
         """Define test variables and initialize app."""
-        # TODO: AHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH
         super().setUp()
+        # TODO: AHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH
+        # Connection.pgSQLconnect()
         self.app = server.app
         self.client = self.app.test_client
-        self.sample_data = { 'firstname': 'John', 'lastname': 'Doe', 'city': 'Pyongyang' }
+        self.sample_data = {'firstname': 'John', 'lastname': 'Doe', 'city': 'Pyongyang'}
 
         # binds the app to the current context
         with self.app.app_context():
@@ -30,17 +31,17 @@ class TestServerApp(unittest.TestCase):
 
     def test_api_can_add_data(self):
         """Api should add data to a table (POST)."""
-        response = self.client().post('/cannabisdb/', self.data)
+        response = self.client().post('/cannabisdb/', data=self.data)
         self.assertEqual(response.status_code, 200)
 
     def test_api_can_reject_missing_data_when_adding(self):
         """Api should reject when user attempts to add data with missing fields. (POST)"""
-        response = self.client().post('/cannabisdb/', { 'firstname': 'John' })
+        response = self.client().post('cannabisdb/', data={'firstname': 'John'})
         self.assertEqual(response.status_code, 400)
 
     def test_api_can_reject_invalid_data_when_adding(self):
         """Api should reject when user attempts to add data with invalid fields. (POST)"""
-        response = self.client().post('/cannabisdb/', { 'first': 'John' })
+        response = self.client().post('/cannabisdb/', data={'first': 'John'})
         self.assertEqual(response.status_code, 400)
 
     def test_api_can_reject_empty_data_when_adding(self):
@@ -49,10 +50,9 @@ class TestServerApp(unittest.TestCase):
         self.assertEqual(response.status_code, 400)
 
     def test_api_can_edit_data(self):
-        """Api should edit data (PUT)."""
-        new_data = { 'firstname': 'Jane', 'lastname': 'Smith', 'city': 'Kampala' }
-        response = self.client().put('/cannabisdb/', id=99, data=new_data)
-        self.assertEqual(response.status_code, 200)
+        """Api should edit data (PUT)"""
+        addResponse = self.client().post('/cannabisdb/', data=self.data)
+        self.assertEqual(addResponse.status_code, 200)
 
     def tearDown(self):
         """teardown all initialized variables."""
@@ -64,3 +64,7 @@ class TestServerApp(unittest.TestCase):
             # drop all tables
             db.session.remove()
             db.drop_all()
+
+# Make the tests conveniently executable
+if __name__ == "__main__":
+    unittest.main()
