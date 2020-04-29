8922278b70ec26dfa15e515891f7807a56915685
smitchell1980 <smm9@pdx.edu>
2020-04-28 19:09:54 -0700

    Cap 68  cursor survival (#21)
    * CAP-68 #comment update driver.py
    
    added rollback feature to current functions that execute against the DB.
    
    * CAP-68 #comment update driver/server
    
    Added flag to the insert_row to track when doing single insert.  Used to commit a single row to the DB.  Updated PUT call to include new flag.
    
    * Update driver.py
    
    * roll in new master branch
    
    * Update driver.py
    
    Added reconnectDB and check_conn functions.  Modified all functions that utilize the cur/conn to DB to reconnect if disconnected.  Modified insert_row with new arg.
    
    * Update driver.py
    
    Update process_file to check connection before trying to commit changes
    
    * Update driver.py
    
    update to process_file function
    
    * Formatting
    
    * Update driver.py
    
    Somehow the insert metadata command was in the function twice.  It was causing issues because the 1st occurance didn't have the new rows/columns args for insertion
    
    Co-authored-by: anhaley <andrew13haley@gmail.com>

--- driver.py
+++ driver.py
@@ -9,11 +9,12 @@ import time
 from openpyxl import load_workbook
 from validation import validate_data_file
 
-
 test_file = 'resources/sample.xlsx'
 primary_table = 'intake'
 metadata_table = 'metadata'
+connection_error_msg = 'The connection to the database is closed and cannot be opened. Verify DB server is up.'
 
+# TODO: refactor to remove duplicated code
 is_connected = False
 wait_time = 0
 while not is_connected:
@@ -24,6 +25,61 @@ while not is_connected:
         time.sleep(1)
         wait_time += 1
         sys.stdout.write(f'\rConnecting to DB ... {wait_time}')
+
+
+def reconnectDB():
+    """
+    Function to reconnect to the database if connection is closed for some reason
+    Args:   None
+    Return (bool): True if connection successful, else False
+    """
+    wait_time = 0
+    global pgSqlConn, pgSqlCur
+    global is_connected
+    is_connected = False
+    while not is_connected:
+        try:
+            pgSqlCur, pgSqlConn = c.pg_connect()
+            is_connected = True
+            return is_connected
+        except:
+            time.sleep(1)
+            wait_time += 1
+            sys.stdout.write(f'\rConnecting to DB ... {wait_time}')
+            if wait_time >= 30:
+                return False
+    return True
+
+
+def check_conn():
+    """
+    Function to check the connection to the database and reconnect if closed
+    Args:  None
+    Return (bool): True if connected, False if connection could not be established
+    """
+    if not is_connected:
+        return reconnectDB()
+    # check to make sure that the connection is open and active
+    try:
+        pgSqlCur.execute('SELECT 1')
+        return True
+    except:
+        return reconnectDB()
+
+
+def sql_except(err):
+    """
+    Function to print out the error message generated from the exception
+    Args:
+        err - the error message generated
+    Returns None
+    """
+    # get the details for exception
+    err_type, err_obj, traceback = sys.exc_info()
+
+    # print the connect() error
+    sys.stderr.write(f"\npsycopg2 ERROR: {err}")
+    return "psycopg2 ERROR:", err
 
 
 def fmt(s):
@@ -49,19 +105,35 @@ def dump_tables():
     Displays the contents of the tables in the database.
     Returns: None
     """
+    # check to make sure that the connection is open and active
+    if not check_conn():
+        return connection_error_msg
+
     print("Test:\n-------------------------------")
-    pgSqlCur.execute(f"select * from {primary_table}")
-    rows = pgSqlCur.fetchall()
-    for row in rows:
-        print(row)
+    try:
+        pgSqlCur.execute(f"select * from {primary_table}")
+        rows = pgSqlCur.fetchall()
+        for row in rows:
+            print(row)
+    except Exception as err:
+        # print the exception
+        sql_except(err)
+        # roll back the last sql command
+        pgSqlCur.execute("ROLLBACK")
 
     print("\nMetadata:\n-------------------------------")
-    pgSqlCur.execute("select * from metadata")
-    rows = pgSqlCur.fetchall()
-    for row in rows:
-        print(row)
-        
-        
+    try:
+        pgSqlCur.execute("select * from metadata")
+        rows = pgSqlCur.fetchall()
+        for row in rows:
+            print(row)
+    except Exception as err:
+        # print the exception
+        sql_except(err)
+        # roll back the last sql command
+        pgSqlCur.execute("ROLLBACK")
+
+
 def table_exists(cur, table):
     """
     Checks whether the named table exists.
@@ -71,6 +143,9 @@ def table_exists(cur, table):
     Returns (bool): whether the table was found
 
     """
+    # check to make sure that the connection is open and active
+    if not check_conn():
+        return connection_error_msg
     try:
         cur.execute("select exists(select relname from pg_class where relname='" + table + "')")
         exists = cur.fetchone()[0]
@@ -79,7 +154,7 @@ def table_exists(cur, table):
         exists = False
 
     return exists
-        
+
 
 # TODO: error handling
 class InvalidTableException(Exception):
@@ -99,22 +174,33 @@ def get_table(table_name):
     result = []
     column_name = []
 
+    # check to make sure that the connection is open and active
+    if not check_conn():
+        result.append(connection_error_msg)
+        return result
+
     if not table_exists(pgSqlCur, table_name):
         raise InvalidTableException
-    
-    pgSqlCur.execute(f"select * from {table_name}")
-    rows = pgSqlCur.fetchall()
-    
-    for col in pgSqlCur.description:
-        column_name.append(col.name)
-    
-    for row in rows:
-        a_row = {}
-        i = 0
-        for col in column_name:
-            a_row[col] = row[i]
-            i += 1
-        result.append(a_row)
+
+    try:
+        pgSqlCur.execute(f"select * from {table_name}")
+        rows = pgSqlCur.fetchall()
+
+        for col in pgSqlCur.description:
+            column_name.append(col.name)
+
+        for row in rows:
+            a_row = {}
+            i = 0
+            for col in column_name:
+                a_row[col] = row[i]
+                i += 1
+            result.append(a_row)
+    except Exception as err:
+        # print the exception
+        sql_except(err)
+        # roll back the last sql command
+        pgSqlCur.execute("ROLLBACK")
 
     return result
 
@@ -164,17 +250,20 @@ def write_info_data(df):
         df (dataframe): data from spreadsheet
     Returns: dict of data writing info
     """
+    # check if the connection is alive
+    if not check_conn():
+        return {'failure': True, 'message': connection_error_msg}
     failed_rows = []
     success_count = 0
     row_array = np.ndenumerate(df.values).iter.base
     total_count = len(row_array)
     for row in row_array:
-        (re, failed_row) = insert_row(primary_table, row)
+        (re, failed_row) = insert_row(primary_table, row, True)
         if re == 1:
             success_count += 1
         else:
             failed_rows.append(failed_row)
-    
+
     return {
         'insertions_attempted': total_count,
         'insertions_successful': success_count,
@@ -190,36 +279,65 @@ def write_metadata(metadata):
     Returns: None
     """
     cmd = "INSERT INTO {}(filename, creator, size, created_date, last_modified_date, last_modified_by, title, rows, columns) " \
-        "VALUES({},{},{},{},{},{},{}, {}, {}) ON CONFLICT DO NOTHING"
-    pgSqlCur.execute(cmd.format(metadata_table, metadata['filename'], metadata['creator'], metadata['size'],
-                                metadata['created'], metadata['modified'], metadata['lastModifiedBy'], metadata['title'],
-                                metadata['rows'], metadata['columns']))
+        "VALUES(" + "{}" * 9 + ") ON CONFLICT DO NOTHING"
 
 
-def insert_row(table, row):
+    # check to make sure that the connection is open and active
+    if not check_conn():
+        return connection_error_msg
+
+    try:
+        pgSqlCur.execute(cmd.format(metadata_table, metadata['filename'], metadata['creator'], metadata['size'],
+                                    metadata['created'], metadata['modified'], metadata['lastModifiedBy'],
+                                    metadata['title'],
+                                    metadata['rows'], metadata['columns']))
+        pgSqlConn.commit()
+
+    except Exception as err:
+        # print the exception
+        sql_except(err)
+        # roll back the last sql command
+        pgSqlCur.execute("ROLLBACK")
+
+
+# TODO: implement multi-row insertion
+def insert_row(table, row, checked=False):
     """
     Insert an array of values into the specified table.
     Args:
         table (str): name of table to insert into
-        row ([]): row of values to insert
+        row ([]): row of values to insert, default to false,
+        checked (bool): flag that connection to DB has already been checked by calling function
     Returns: (bool, dict) a bool indicate whether insertion is successful, a dict of failed row info
 
     """
+    # Check flag for multi row insert, if false check to make sure that the connection is open and active
+    if not checked:
+        if not check_conn():
+            return 0, connection_error_msg
+
     cmd = f"INSERT INTO {table} VALUES (DEFAULT"
     for i in range(1, len(row)):
         cmd += f", {fmt(row[i])}"
     cmd += ")"
-    pgSqlCur.execute(cmd)
+    try:
+        pgSqlCur.execute(cmd)
 
-    if pgSqlCur.rowcount == 1:
-        return 1, None
-    else:
-        failed_row = {
-            'submission_date': row[1],
-            'entity': row[2],
-            'dba': row[3]
-        }
-        return 0, failed_row
+        if pgSqlCur.rowcount == 1:
+            return 1, None
+        else:
+            failed_row = {
+                'submission_date': row[1],
+                'entity': row[2],
+                'dba': row[3]
+            }
+            return 0, failed_row
+
+    except Exception as err:
+        # print the exception
+        sql_except(err)
+        # roll back the last sql command
+        pgSqlCur.execute("ROLLBACK")
 
 
 def process_file(f):
@@ -229,29 +347,29 @@ def process_file(f):
         f (str): filename of spreadsheet
     Returns (bool, dict): bool is successful or not, dict includes processing info 
     """
+    # check to make sure that the connection is open and active
+    if not check_conn():
+        return 0, connection_error_msg
+    else:
+        # read file content
+        df = load_spreadsheet(f)
+        # Validate data frame
+        valid, error_msg = validate_data_file(df)
+        if not valid:
+            return False, {'status': 'invalid', 'error_msg': error_msg}
+        # Write the data to the DB
+        result_obj = write_info_data(df)
+        # insert metadata into metadata table
+        # should add version and revision to this schema, but don't know types yet
+        metadata = read_metadata(f)
 
-    # read file content
-    df = load_spreadsheet(f)
+        write_metadata(metadata)
 
-    # Validate data frame
-    valid, error_msg = validate_data_file(df)
-    if not valid:
-        return False, {'status': 'invalid', 'error_msg': error_msg}
-    
-    # Write the data to the DB
-    result_obj = write_info_data(df)
+        # commit execution
+        pgSqlConn.commit()
 
-    # insert metadata into metadata table
-    # should add version and revision to this schema, but don't know types yet
-    metadata = read_metadata(f)
-    
-    write_metadata(metadata)
-
-    # commit execution
-    pgSqlConn.commit()
-
-    failed_insertions = result_obj['insertions_attempted'] - result_obj['insertions_successful']
-    return failed_insertions == 0, result_obj
+        failed_insertions = result_obj['insertions_attempted'] - result_obj['insertions_successful']
+        return failed_insertions == 0, result_obj
 
 
 def test_driver():
@@ -285,4 +403,4 @@ def test_driver():
 
 if __name__ == '__main__':
     test_driver()
-    print(get_table('test'))
+    print(get_table({primary_table}))
