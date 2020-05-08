21a88ff8f758353bc6e7c8cc49582db167a7c1e0
anhaley <andrew13haley@gmail.com>
2020-05-07 21:30:59 -0700

    Refactor; rename test files

--- query_parser.py
+++ query_parser.py
@@ -1,6 +1,8 @@
 import json
+from models import IntakeRow
 
 db_tables = ['intake', 'txn_history', 'archive', 'metadata']
+col_names = [x.name.lower() for x in IntakeRow.ColNames]
 
 
 def invalid_request_msg(e):
@@ -39,14 +41,20 @@ def parse_op(op_block):
         return parse_or(op_block.get('or'))
     # basic operator
     op = op_block.get('op')
-    if op == '<':
-        pass
-    elif op == '>':
-        pass
-    elif op == '=':
-        pass
+    if op in ['<', '>', '=']:
+        col = op_block.get('column')
+        validate_column(col)
+        operand = op_block.get('operand')
+        # TODO: this is going to be a problem since we're quoting everything
+        # TODO: need to account for numeric/null values
+        return f" {col} {op} '{operand}'"
     else:
         raise RequestParseException(unknown_operator_msg(op))
+
+
+def validate_column(col):
+    if col.lower() not in col_names:
+        raise RequestParseException(invalid_column_msg(col))
 
 
 def build_query(q):
@@ -56,23 +64,23 @@ def build_query(q):
         if table not in db_tables:
             raise RequestParseException(invalid_table_msg(table))
 
-        col_names = []
-        if table == 'intake':
-            from models import IntakeRow
-            col_names = [x.name.lower() for x in IntakeRow.ColNames]
+        # col_names = []
+        # if table == 'intake':
+            # from models import IntakeRow
+            # TODO: transition to class, use this to set instance col_names
+            # col_names = [x.name.lower() for x in IntakeRow.ColNames]
 
         # if columns not listed, assume all
         columns = q.get('columns', '*')
         if columns != '*':
             for col in columns:
-                if col.lower() not in col_names:
-                    raise RequestParseException(invalid_column_msg(col))
+                validate_column(col)
 
         query = f"SELECT {','.join(columns)} FROM {table} "
         # get extended filtering
         where = q.get('where')
         if where is not None:
-            query += 'WHERE '
+            query += 'WHERE'
             # iterate over operators
             query += parse_op(where)
 
@@ -83,7 +91,7 @@ def build_query(q):
 
 
 if __name__ == '__main__':
-    with open('resources/test-query-fail-1.json', 'r') as f:
+    with open('resources/test-query-equals.json', 'r') as f:
         body = json.load(f)
     q = build_query(body)
     print(q)
