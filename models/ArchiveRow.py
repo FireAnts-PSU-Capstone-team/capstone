e83e774ce585ab5861572e1cea9dff425ce925ba
smitchell1980 <smm9@pdx.edu>
2020-05-11 16:43:48 -0700

    Cap 94/95   model reports/violations table (#39)
    Added new tables, renaming functions for clearer meanings.  Added new functions and triggers for new tables.  Updated archive table with new columns

--- models/ArchiveRow.py
+++ models/ArchiveRow.py
@@ -8,33 +8,9 @@ class ArchiveRow:
         Default constructor.
         """
         self.row = None
-        self.old_row = None
-        self.old_submission_date = ''
-        self.old_entity = ''
-        self.old_dba = ''
-        self.old_facility_address = ''
-        self.old_facility_suite = ''
-        self.old_facility_zip = ''
-        self.old_mailing_address = ''
-        self.old_mrl = ''
-        self.old_neighborhood_assn = ''
-        self.old_compliance_region = ''
-        self.old_first_name = ''
-        self.old_last_name = ''
-        self.old_email = ''
-        self.old_phone = ''
-        self.old_endorse_type = ''
-        self.old_license_type = ''
-        self.old_repeat_license = ''
-        self.old_app_complete = ''
-        self.old_fee_schedule = ''
-        self.old_receipt_num = ''
-        self.old_cash_amt = ''
-        self.old_check_amt = ''
-        self.old_card_amt = ''
-        self.old_check_num = ''
-        self.old_mrl_num = ''
-        self.old_notes = ''
+        self.tstamp = ''
+        self.who =''
+        self.old_val=''
 
     def __init__(self, input_json):
         """
@@ -43,31 +19,8 @@ class ArchiveRow:
             input_json ({}): JSON object representing a row of the archive table
         """
         self.row = input_json.get('row')
-        self.old_row = input_json.get('old_row')
-        self.old_submission_date = input_json.get('old_submission_date')
-        self.old_entity = input_json.get('old_entity')
-        self.old_dba = input_json.get('old_dba')
-        self.old_facility_address = input_json.get('old_facility_address')
-        self.old_facility_suite = input_json.get('old_facility_suite')
-        self.old_facility_zip = input_json.get('old_facility_zip')
-        self.old_mailing_address = input_json.get('old_ailing_address')
-        self.old_mrl = input_json.get('old_mrl')
-        self.old_neighborhood_assn = input_json.get('old_neighborhood_association')
-        self.old_compliance_region = input_json.get('old_compliance_region')
-        self.old_first_name = input_json.get('old_primary_contact_first_name')
-        self.old_last_name = input_json.get('old_primary_contact_last_name')
-        self.old_email = input_json.get('old_email')
-        self.old_phone = input_json.get('old_phone')
-        self.old_endorse_type = input_json.get('old_endorse_type')
-        self.old_license_type = input_json.get('old_license_type')
-        self.old_repeat_license = input_json.get('old_repeat_location')
-        self.old_app_complete = input_json.get('old_app_complete')
-        self.old_fee_schedule = input_json.get('old_fee_schedule')
-        self.old_receipt_num = input_json.get('old_receipt_num')
-        self.old_cash_amt = input_json.get('old_ash_amount')
-        self.old_check_amt = input_json.get('old_check_amount')
-        self.old_card_amt = input_json.get('old_card_amount')
-        self.old_check_num = input_json.get('old_check_num_approval_code')
-        self.old_mrl_num = input_json.get('old_mrl_num')
-        self.old_notes = input_json.get('old_notes')
+        self.tstamp = input_json.get('tstamp')
+        self.who = input_json.get('who')
+        self.old_val = input_json.get('old_val')
+
 
---
+++ models/ViolationsRow.py
@@ -0,0 +1,87 @@
+from enum import Enum
+
+
+class RowNames(Enum):
+    ROW = 0
+    DBA = 1
+    ADDRESS = 2
+    MRL = 3
+    LICENSE_TYPE = 4
+    VIOLATION_SENT_DATE = 5
+    ORIGINAL_VIOLATION_AMOUNT = 6
+    ADMIN_RVW_DECISION_DATE = 7
+    ADMIN_RVW_VIOLATION_AMOUNT = 8
+    CERTIFIED_NUM = 9
+    CERTIFIED_RECEIPT_RETURNED = 10
+    DATE_PAID_WAIVED = 11
+    RECEIPT_NUM = 12
+    CASH_AMT = 13
+    CHECK_AMT = 14
+    CARD_AMT = 15
+    CHECK_NUM = 16
+    NOTES = 17
+
+
+class ViolationsRow:
+    """
+    Represents a row of the violations table.
+    """
+
+    def __init__(self):
+        """
+        Default constructor.
+        """
+        self.row_id = None
+        self.dba = ''
+        self.address = ''
+        self.mrl = ''
+        self.license_type = ''
+        self.violation_sent_date = ''
+        self.original_violation_amount = ''
+        self.admin_rvw_decision_date = ''
+        self.admin_rvw_violation_amount = ''
+        self.certified_num = ''
+        self.certified_reciept_returned = ''
+        self.date_paid_waived = ''
+        self.receipt_no = ''
+        self.cash_amt = ''
+        self.check_amt = ''
+        self.card_amt = ''
+        self.check_num = ''
+        self.notes = ''
+
+    def __init__(self, input_json):
+        """
+        Constructor from a JSON object. Any unsupplied data defaults to None
+        Args:
+            input_json ({}): JSON object representing a row of the intake table
+        """
+        self.row_id = input_json.get('row_id')
+        self.dba = input_json.get('DBA')
+        self.address = input_json.get('Address')
+        self.mrl = input_json.get('MRL#')
+        self.license_type = input_json.get('License Type')
+        self.violation_sent_date = input_json.get('Violation sent date')
+        self.original_violation_amount = input_json.get('Original Violation Amount')
+        self.admin_rvw_decision_date = input_json.get('Admin Rvw Decision Date')
+        self.admin_rvw_violation_amount = input_json.get('Admin Rvw Violation Amount')
+        self.certified_num = input_json.get('Certified #')
+        self.certified_reciept_returned = input_json.get('Certified receipt returned')
+        self.date_paid_waived = input_json.get('Date paid/ Waived')
+        self.receipt_no = input_json.get('Receipt No.')
+        self.cash_amt = input_json.get('Cash Amount')
+        self.check_amt = input_json.get('Check Amount')
+        self.card_amt = input_json.get('Card Amount')
+        self.check_num = input_json.get('Check No. / Approval Code')
+        self.notes = input_json.get('Notes')
+
+    def value_array(self):
+        """
+        Order data into an array that can be consumed by the row insertion method.
+        Returns ([]): array of data members
+        """
+        return [self.row_id, self.dba, self.address, self.mrl, self.license_type, self.violation_sent_date,
+                self.original_violation_amount, self.admin_rvw_decision_date, self.admin_rvw_violation_amount,
+                self.certified_num, self.certified_reciept_returned,
+                self.date_paid_waived, self.receipt_no, self.cash_amt,
+                self.check_amt, self.card_amt, self.check_num, self.notes]
