aaab55d7ed0dce531058d91113ddadf10580b46c
Sean Mitchell <smm9@pdx.edu>
2020-05-25 02:54:41 -0700

    SeqUpdating
    driver.py - added row seq dictionary
    loader.py - new functions for reading and writing the seqs to ini file for reloading if server crashes
    row_seq_counts.ini - file to hold row seq counts

--- loader.py
+++ loader.py
@@ -1,15 +1,18 @@
 from configparser import ConfigParser
 
-def loadSeqCounts(filename, tables, seqs):
+def loadseqcounts(filename, seqs):
 # create a parser
     parser = ConfigParser()
     # read config file
     parser.read(filename)
-
-    # get section, default to postgresql
-
     if parser.has_section('row_sequences'):
         params = parser.items('row_sequences')
         for param in params:
-            seqs[param[0]] = param[1]
+            seqs[param[0]] = int(param[1])
 
+
+def writeseqcounts(filename, seqs):
+    parser = ConfigParser()
+    parser['row_sequences']=seqs
+    with open(filename, 'w') as configfile:
+        parser.write(configfile)
