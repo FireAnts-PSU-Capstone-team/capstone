a5062aed1428048e8f33aee60c555293df7df878
Lawrence-Gunnell <36523041+Lawrence-Gunnell@users.noreply.github.com>
2020-05-31 15:27:30 -0700

    Cap 123 cors issues with gui bp (#68)
    * CAP-123 #comment Registers blueprints with CORS
    
    * CAP-123 #comment Registers blueprints with CORS and prints list of accepted domains
    
    * CAP-123 #comment Added CORS domains to accepted_domains.ini
    
    * CAP-123 #comment Manually adds necessary headers needed for CORS requests. This inclusion might be redundant due to flask-cors handling endpoint blueprints. These can be scaled back as appropriate once the issue is confirmed to have been resolved.
    
    * CAP-123 #comment Removes redundant header processing including the prior header issue due to a malformed string.
    
    * CAP-123 #comment Error-Catching decorator added that includes proper CORS headers when a server error is encountered. Decorator has been added to the /load endpoint.
    
    * CAP-123 #comment Fixes authentication issue where failed GUI login attempts resulted in 200 OK messages instead of 400 ERROR messages. Curl documentation also added.
    
    * More CORS fun
    
    Signed-off-by: Connor Kaz <connor4@pdx.edu>
    
    Co-authored-by: Connor Kaz <connor4@pdx.edu>

--- kanabi/responses.py
+++ kanabi/responses.py
@@ -1,9 +1,47 @@
 from flask import make_response, jsonify, session
 from flask_login import current_user, AnonymousUserMixin
+from .cors import cors_setup
+
+origin_list = []
 
 
 # Makes a response formatted for use with the frontend GUI
 def make_gui_response(headers, return_code, status_msg):
+    '''
+    Proper responses that pass CORS should contain the following headers:
+    Access-Control-Allow-Headers = Authorize  (used in browser's pre-flight request)
+    Access-Control-Allow-Origin = FULLY_QUALIFIED_DOMAIN_NAME (must match Origin header)
+    Access-Control-Allow-Credentials = True  (required if cookies are used for login)
+
+    Test Curls:
+    $ curl -k -i -X OPTIONS -H "Origin: https://ALLOWED_DOMAIN_NAME" -H 'Access-Control-Request-Method: POST'
+           -H 'Access-Control-Request-Headers: Content-Type, Authorization' "https://DOMAIN_NAME:PORT/ENDPOINT"
+
+    Successful Response (Passes CORS):
+    HTTP/1.0 200 OK
+    Content-Type: text/html; charset=utf-8
+    Allow: GET, HEAD, PUT, OPTIONS, POST
+    Access-Control-Allow-Origin: https://kanabi-gui.herokuapp.com
+    Access-Control-Allow-Headers: Authorization, Content-Type
+    Access-Control-Allow-Methods: DELETE, GET, HEAD, OPTIONS, PATCH, POST, PUT
+    Vary: Origin, Cookie
+    Set-Cookie: session=.eJyrVspMSc0rySyp1EssLcmIL6ksSFWyyivNydFByGSmQIRqAZu4EWc.Xs8_-g.HQTLJSSfqKETLQPPuceCn8sqG7o; HttpOnly; Path=/
+    Content-Length: 0
+    Server: Werkzeug/1.0.1 Python/3.6.10
+    Date: Thu, 28 May 2020 04:37:14 GMT
+
+    Failed Response (Doesn't pass CORS)
+    HTTP/1.0 200 OK
+    Content-Type: text/html; charset=utf-8
+    Allow: GET, HEAD, PUT, OPTIONS, POST
+    Vary: Cookie
+    Set-Cookie: session=.eJyrVspMSc0rySyp1EssLcmIL6ksSFWyyivNydFByGSmQIRqAZu4EWc.Xs9Bxg.rM9DEBnYHgdKgrIcOTUNze1PMZc; HttpOnly; Path=/
+    Content-Length: 0
+    Server: Werkzeug/1.0.1 Python/3.6.10
+    Date: Thu, 28 May 2020 04:44:54 GMT
+    '''
+
+    global origin_list
     if 'read_only_mode' not in session:
         session['read_only_mode'] = False
 
@@ -13,5 +51,12 @@ def make_gui_response(headers, return_code, status_msg):
     else:
         user_dict = {'logged_in': False}
 
-    response_body = {'headers': headers, 'return_msg': status_msg, 'user': user_dict}
-    return make_response(jsonify(response_body), return_code)
+    response_body = {'return_msg': status_msg, 'user': user_dict}
+    response = make_response(jsonify(response_body), return_code)
+    return response
+
+# Loads the domain whitelist from file and provides access to origin_list through import functionality.
+def update_origin_list():
+    global origin_list
+    origin_list = cors_setup.load_domains_from_file()
+    return origin_list
